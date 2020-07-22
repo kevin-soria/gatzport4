7fddf3202e1ce944ed4e463602e7a9a67f2a067e
Kevin Soria <Kevin.Soria@outlook.com>
2020-07-22 19:12:09 -0400

    Gatsby integration

---
+++ portfoliogatsby/gatsby-starter-dimension/src/components/Header.js
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Kevin Soria</h1>
+        <p>
+          A Portfolio Website for showcasing me  {' '}
+          <a href="https://github.com/kevin-soria">Software Engineer</a>
+          <br />
+          available for hire under the{' '}
+          <a href="https://www.linkedin.com/in/kevin-soria/">Creative Commons</a> license.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            About Me
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
