0b4ec528792bf58d3ee5913ae371f25a180dabe0
Kevin Soria <Kevin.Soria@outlook.com>
2020-09-03 18:53:56 -0400

    to deploy

--- gatzport4/gatsby-startbootstrap-agency/config/site.js
+++ gatzport4/gatsby-startbootstrap-agency/config/site.js
@@ -10,8 +10,8 @@ module.exports = {
     "bootstrap",
     "startbootstrap-agency",
   ],
-  description: "gatsby version of startbootstrap-agency with i18n support.",
-  author: "thundermiracle",
+  description: "Portfolio Website of Kevin Soria",
+  author: "Kevin Soria",
   trackingId: "UA-119418003-5",
   defaultLang: "en",
   langTextMap: {
